26162d47878c339f632824c2f291236760090b86
theV1rTual <arystandev@gmail.com>
2026-06-22 17:50:47 +0500

    hw6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        let value = restoreState<string>('hw6-editable-span-value', '')
+        setValue(value);
     }
 
     return (
